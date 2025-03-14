2647e5c1306153c503bd5afdaef5433a599246c2
pratik pakhale <pratikpakhale20@gmail.com>
2025-03-14 20:54:56 +0530

    build: fix next config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import { get } from '@/sanity/lib/client';
-import { GetAllFaculties } from '@/sanity/lib/queries';
-import { GetAllFacultiesResult } from '@/sanity/types';
-
 export default async function Home() {
-  // example usage
-  const data = await get<GetAllFacultiesResult>(GetAllFaculties);
-  console.log(data);
-
   return <div>hi</div>;
 }
