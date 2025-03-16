f5c2b73d0558a0cbb542e3dbf222e25ddef6689c
Nikhil Karthik <nikhilkarthik241103@gmail.com>
2025-03-16 19:26:37 +0530

    feat: placements and about section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
+import AboutPlacements from '@/components/home/about-placement';
 import Alumni from '@/components/home/alumni';
 import LandingSection from '@/components/home/landing-section';
 import Notification from '@/components/home/notification';
 import SocialMedia from '@/components/home/social-media';
+import VisionMission from '@/components/home/vision-mission';
 import ToTop from '@/components/to-top';
 
 export default async function Home() {
   return (
     <main className="min-h-screen">
       <LandingSection />
+      <AboutPlacements />
+      <VisionMission />
       <Notification />
       <Alumni />
       <SocialMedia />
+
       <ToTop />
     </main>
   );
