7a02741403f05979f17c386fa49c0d302319cf15
Ashith Shetty <ashithdshetty1101@gmail.com>
2025-03-31 22:10:07 +0530

    placements page

---
+++ src/app/placements/pastRecruiters.tsx
@@ -0,0 +1,261 @@
+export interface Recruiter {
+  name: string;
+}
+
+export const pastRecruiters: Recruiter[] = [
+  // A
+  { name: "Aakash Byju's" },
+  { name: 'Accenture' },
+  { name: 'Accolite' },
+  { name: 'Act Fibernet' },
+  { name: 'Adani Financials' },
+  { name: 'Addverb Technologies' },
+  { name: 'Aditya Birla Capital' },
+  { name: 'Affordable Technologies Pvt. Ltd.' },
+  { name: 'Algoanalytics' },
+  { name: 'Alignminds' },
+  { name: 'Allstate Health Plan' },
+  { name: 'Amazon' },
+  { name: 'Amazon (Prime)' },
+  { name: 'Amuse Labs' },
+  { name: 'Anmol Labs Pvt. Ltd.' },
+  { name: 'Aquatexx' },
+  { name: 'Atlassian' },
+
+  // B
+  { name: 'Beebly Software Solutions Pvt.Ltd' },
+  { name: 'Bentley Runs Inside View Technologies' },
+  { name: 'Bosch Global Software Technologies' },
+  { name: 'Brane Enterprises(NU)' },
+  { name: 'Bridgelabz' },
+  { name: "Byju's" },
+
+  // C
+  { name: 'Capgemini' },
+  { name: 'Care Health Insurance Limited' },
+  { name: 'C-DAC' },
+  { name: 'CGI' },
+  { name: 'Codemantra Technologies Pvt. Ltd.' },
+  { name: 'Codemotion Innovation Labs' },
+  { name: 'Cognizant' },
+  { name: 'Copywriter' },
+  { name: 'Coruscant' },
+  { name: 'Credflow' },
+  { name: 'Curefit' },
+  { name: 'Cvent' },
+  { name: 'Cyient' },
+  { name: 'Cyient Global' },
+  { name: 'Cyient Solutions' },
+  { name: 'Cuvette' },
+
+  // D
+  { name: 'Data Insights' },
+  { name: 'De Shaw' },
+  { name: 'Deloitte' },
+  { name: 'Dotpay' },
+  { name: 'Demandfarm' },
+  { name: 'Dunzo' },
+  { name: 'Duropad' },
+  { name: 'Dvara Solutions' },
+  { name: 'Dynus' },
+
+  // E
+  { name: 'Eclerx' },
+  { name: 'ElectricPe' },
+  { name: 'Electroneek' },
+  { name: 'Engineers Circle' },
+  { name: 'Ericsson India Global Services' },
+  { name: 'Ericsson Bangalore' },
+  { name: 'Essilor' },
+  { name: 'Exotel' },
+  { name: 'Extramarks' },
+  { name: 'Ezest Edtech Pvt. Ltd.' },
+  { name: 'Expand My Business' },
+
+  // F
+  { name: 'Facet Prep' },
+  { name: 'Fidelity Software Pvt. Ltd.' },
+  { name: 'Finway' },
+  { name: 'Fis University' },
+  { name: 'Flipkart' },
+
+  // G
+  { name: 'GoWiz Inc.' },
+  { name: 'Growwscore' },
+  { name: 'Greensync' },
+  { name: 'Gridlogics' },
+  { name: 'GRM' },
+  { name: 'Growth Jockey India Limited' },
+
+  // H
+  { name: 'Hexaware' },
+  { name: 'Hike' },
+  { name: 'Human Analogix' },
+
+  // I
+  { name: 'Itron Technologies' },
+  { name: 'Ivy Mobility' },
+  { name: 'Ivy Bangalore' },
+  { name: 'Ixceed' },
+  { name: 'Incture' },
+  { name: 'Indiamart' },
+  { name: 'Indian School Of Development Management' },
+  { name: 'Infosys' },
+  { name: 'Information Sharing And Analysis Centre' },
+  { name: 'Informatica' },
+  { name: 'Infrrd India' },
+  { name: 'Intellect Design Arena Ltd.' },
+  { name: 'Interleaf' },
+  { name: 'Internshala' },
+  { name: 'Interview Kickstart Calypsoid' },
+  { name: 'Invacture' },
+  { name: 'Iovus' },
+  { name: 'Iron-Calypsoid' },
+
+  // J
+  { name: 'Jio Platforms Limited' },
+  { name: 'Josh Technology Group' },
+
+  // K
+  { name: 'K12 Techno Services Pvt Ltd' },
+  { name: 'Knowledge Foundry' },
+
+  // L
+  { name: 'L&T Infotech' },
+  { name: 'Lido Games' },
+  { name: 'Lovely Juggernaut Inc.' },
+  { name: 'LTIMindtree' },
+
+  // M
+  { name: 'Mahindra & Mahindra' },
+  { name: 'Map Software' },
+  { name: 'Mavenir' },
+  { name: 'Max Secure Software' },
+  { name: 'Mindtree' },
+  { name: 'Mirketa' },
+  { name: 'Mobisy' },
+  { name: 'Microsoft Research' },
+  { name: 'Mindtree' },
+  { name: 'Mitra Solutions' },
+  { name: 'Molekule' },
+  { name: 'Moneytap' },
+  { name: 'Move InSync' },
+  { name: 'Mu Sigma' },
+  { name: 'Mycprotain' },
+  { name: 'MyWays.AI' },
+
+  // N
+  { name: 'Navgurukul' },
+  { name: 'Neosoft Technologies' },
+  { name: 'Newgen Software Technologies' },
+  { name: 'Nessum' },
+  { name: 'Niramai' },
+  { name: 'Number Theory' },
+  { name: 'Numeric Technologies India Pvt Ltd' },
+  { name: 'Nutrade' },
+  { name: 'Nutanix' },
+  { name: 'Nurture (Thoughtworks)' },
+
+  // O
+  { name: 'O9 Solutions' },
+  { name: 'Oracle' },
+  { name: 'Outlook Group' },
+
+  // P
+  { name: 'Patriot Technology Solutions' },
+  { name: 'Polestar' },
+  { name: 'Polymath Tech Pvt Ltd' },
+  { name: 'Pricewaterhouse' },
+  { name: 'Prozeal' },
+  { name: 'Pune Spark' },
+  { name: 'Purplle' },
+  { name: 'Proxymed' },
+  { name: 'Proselina' },
+  { name: 'Publicis Sapient' },
+  { name: 'Pyzaar' },
+
+  // Q
+  { name: 'Qube Tales' },
+  { name: 'Quince Calypsoid' },
+
+  // R
+  { name: 'R Systems International Limited' },
+  { name: 'RailYatri.in' },
+  { name: 'Real Ventures Inc.' },
+  { name: 'Recruit CRM' },
+  { name: 'Redseer Strategy Consultants' },
+  { name: 'Revature' },
+  { name: 'Ridiv Technologies' },
+  { name: 'Ritwik' },
+
+  //S
+  { name: 'Salesforce' },
+  { name: 'Samsung Data Systems' },
+  { name: 'Semifront Technologies Private Limited' },
+  { name: 'Shipx' },
+  { name: 'Shopconnect - Nvipani Technology' },
+  { name: 'Siemens' },
+  { name: 'Skillbuz' },
+  { name: 'Smartcoin Financials' },
+  { name: 'Soctronics' },
+  { name: 'Softech Engineers Limited.' },
+  { name: 'Spottabl (Only Internship)' },
+  { name: 'Springml' },
+  { name: 'Structured' },
+  { name: 'Subex' },
+  { name: 'Supaki Inc' },
+  { name: 'Swiggy' },
+  { name: 'Synapse Techno Design Innovations Pvt Ltd' },
+  { name: 'Synopsys' },
+
+  //T
+  { name: 'Talentserve' },
+  { name: 'Tata Iq' },
+  { name: 'Tata Motors Limited' },
+  { name: 'Tavant' },
+  { name: 'Tcs' },
+  { name: 'Tech Mahindra' },
+  { name: 'Techolution' },
+  { name: 'Techurate' },
+  { name: 'Tejas Network' },
+  { name: 'Tensorgo Software Pvt Ltd.' },
+  { name: 'Tesca Technologies Pvt. Ltd.' },
+  { name: 'Tesco' },
+  { name: 'Teslabs' },
+  { name: 'The Jurni' },
+  { name: 'Thirdeye Data' },
+  { name: 'Thought Genesis' },
+  { name: 'Thrillophilia' },
+  { name: 'Trilogy Innovations' },
+
+  //U
+  { name: 'Utopia Labs' },
+
+  //V
+  { name: 'Vayavya Labs' },
+  { name: 'Virtusa' },
+
+  //W
+  { name: 'Wells Fargo' },
+  { name: 'Weskill' },
+  { name: 'Wiely-Mthree' },
+  { name: 'Wiley Edge' },
+  { name: 'Winnow Systems' },
+  { name: 'Wipro' },
+  { name: 'Workruit' },
+
+  //X
+  { name: 'Xebia' },
+
+  //Y
+  { name: 'Y B Tradelinks' },
+  { name: 'Yhills Edutech' },
+
+  //Z
+  { name: 'Zensar' },
+  { name: 'Zscaler' },
+  { name: 'Zycus' },
+
+  { name: '314E- Healthcare It' }
+  // ... (Add more if needed)
+];
