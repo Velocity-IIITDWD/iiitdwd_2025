124cc2babe321c4379723d0cf03fd4e58adb17cb
pratik pakhale <pratikpakhale20@gmail.com>
2025-06-23 12:43:48 +0100

    chore: update admission quick links and add grouping logic in staff page

--- src/app/(administration)/staff/page.tsx
+++ src/app/(administration)/staff/page.tsx
@@ -1,4 +1,5 @@
 import { DynamicProfileSections } from '@/components/committee';
+import { groupAndOrderItems } from '@/lib/utils';
 import { get } from '@/sanity/lib/client';
 import { GetAllStaff } from '@/sanity/lib/queries';
 import { GetAllStaffResult } from '@/sanity/types';
@@ -8,6 +9,25 @@ export default async function Page() {
   response.forEach((section) => {
     section.title = 'Staff';
   });
+
+  // custom staff grouping
+  const staffGroups = [
+    {
+      name: 'Leadership',
+      items: ['Dr. K. Gopinath', 'Prof. Vasudev Parvati']
+    }
+  ];
+
+  response.forEach((section) => {
+    if (section.profiles) {
+      section.profiles = groupAndOrderItems(
+        section.profiles,
+        staffGroups,
+        (profile) => profile.title ?? undefined
+      );
+    }
+  });
+
   return (
     <main className="w-[87.5vw] max-w-[1680px] mx-auto">
       <DynamicProfileSections sections={response} />
